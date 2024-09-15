180e5c7c88faca83b771df9fcc9304144a29cb92
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2024-09-15 17:14:35 +0500

    add french language in home and services

--- src/components/servicePageSections/ServiceHero.vue
+++ src/components/servicePageSections/ServiceHero.vue
@@ -4,10 +4,11 @@
     <img src="Rectangle 4817 (1).png" alt="user" class="w-full md:h-[430px] h-[200px]" />
     <!-- service section -->
     <h1 class=" wow animate__animated animate__fadeInDown text-headingclr lg:text-5xl text-4xl font-bold font-bevietnam text-center mt-20">
-      Our Services
+      {{ $t("our_services_title") }}
+      
     </h1>
     <p class=" wow animate__animated animate__fadeInUp font-bevietnam text-center max-w-[1200px] px-2 mx-auto mt-4 font-normal text-lg text-textcolor">
-      At Tech Wizard, we don't just develop software – we craft innovative, custom-tailored solutions that drive your business forward. With a team of seasoned professionals, we specialize in building scalable, efficient, and secure digital products designed to meet the unique needs of each client.
+      {{ $t("our_services_des") }}
     </p>
     
   </div>
